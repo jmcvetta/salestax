a857b9bb5b4e07aa09c3130ebad6401e8587c776
Jason McVetta <jason.mcvetta@gmail.com>
2012-06-29 15:51:50 -0700

    Better formatting

--- salestax.rb
+++ salestax.rb
@@ -19,7 +19,6 @@
 # This code is a solution to the following challenge question:
 #
 #
-#
 # Basic sales tax is applicable at a rate of 10% on all goods, except books,
 # food, and medical products that are exempt. Import duty is an additional
 # sales tax applicable on all imported goods at a rate of 5%, with no
